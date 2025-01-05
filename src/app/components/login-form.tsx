4aae7e507123d88784b315013e310e0a1d1bac79
kojesung <js95112345@gmail.com>
2025-01-05 15:50:18 +0900

    Form 관련 함수 선언

--- src/app/components/login-form.tsx
+++ src/app/components/login-form.tsx
@@ -1,17 +1,51 @@
 'use client';
 
+import { FieldError, useForm } from 'react-hook-form';
+
+interface HookFormTypes {
+    id: string;
+    password: string;
+}
+
 export default function LoginForm() {
+    const { register, handleSubmit } = useForm<HookFormTypes>();
+    const onValid = (data: HookFormTypes) => {
+        console.log(data);
+    };
+    const onInValid = (errors: FieldError) => {
+        console.log(errors);
+    };
+
     return (
         <div>
-            <div className="flex flex-col gap-2">
-                <input type="text" className="border border-gray-300 rounded-md p-2" />
-                <input type="text" className="border border-gray-300 rounded-md p-2" />
-            </div>
-            <div className="my-4">
-                <div>체크박스</div>
-                <div>일치여부</div>
-            </div>
-            <button className=" text-white px-4 py-2 rounded-md bg-[rgba(255,72,105,1)]">로그인</button>
+            <form className="flex flex-col gap-2" onSubmit={handleSubmit(onValid)}>
+                <input
+                    {...register('id', {
+                        required: true,
+                        minLength: 4,
+                        maxLength: { value: 16, message: '에러 메세지 설정' },
+                    })}
+                    placeholder="아이디를 입력하세요"
+                    type="text"
+                    className="border border-gray-300 rounded-md p-2"
+                />
+                <input
+                    {...register('password', { required: true, minLength: 8, maxLength: 12 })}
+                    placeholder="비밀번호를 입력하세요"
+                    type="password"
+                    className="border border-gray-300 rounded-md p-2"
+                />
+                <div className="my-4">
+                    <div>체크박스</div>
+                    <div>일치여부</div>
+                </div>
+                <input
+                    type="submit"
+                    placeholder="로그인"
+                    className=" text-white px-4 py-2 rounded-md bg-[rgba(255,72,105,1)]"
+                />
+            </form>
+
             <div className="mt-4 space-x-2">
                 <button className="text-gray-600">아이디 찾기</button>
                 <button className="text-gray-600">비밀번호 찾기</button>
