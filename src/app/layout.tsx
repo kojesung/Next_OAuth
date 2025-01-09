0c54e18293ecda69a457e42265b856970e2f61c9
kojesung <js95112345@gmail.com>
2025-01-09 14:29:56 +0900

    빌드에러?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navigation from './components/navigation/navigation';
-import { SessionProvider } from 'next-auth/react';
 import AuthProvider from './components/providers/SessionProvider';
 
 const geistSans = Geist({
